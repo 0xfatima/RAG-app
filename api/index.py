41cd6bae8b37850a6dd02660af786b8265d1c3ad
fatima22234 <emaaaa37@gmail.com>
2024-08-15 11:55:32 +0500

    first commit

--- api/index.py
+++ api/index.py
@@ -1,7 +1,152 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+#importing
+from langchain_community.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, WebBaseLoader, YoutubeLoader, DirectoryLoader, TextLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+import numpy as np
+import tiktoken
+from groq import Groq
+import os
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import json
+from typing import List  # Make sure to import List from typing
 
+#loading keys and env variables
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
 app = FastAPI()
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or specify the domains you want to allow
+    allow_credentials=True,
+    allow_methods=["*"],  # or specify the methods you want to allow
+    allow_headers=["*"],  # or specify the headers you want to allow
+)
+
+#initializing groq client to send api request
+client = Groq(api_key=groq_api_key)
+
+# instructions to final model (llama 3.1 8b)
+primer = f"""You are a personal assistant. Answer any questions I have about the Youtube Video provided.
+Translate in specific language if user asks you to
+"""
+
+#loading video from youtube
+loader = YoutubeLoader.from_youtube_url("https://www.youtube.com/watch?v=e-gwvmhyU7A", add_video_info=True)
+data = loader.load()
+
+
+# making chunks of data got from youtube video
+tokenizer = tiktoken.get_encoding('p50k_base')
+
+def tiktoken_len(text):
+    tokens = tokenizer.encode(
+        text,
+        disallowed_special=()
+    )
+    return len(tokens)
+
+#defining text_splitter to make chunks
+text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=100,
+        length_function=tiktoken_len,
+        separators=["\n\n", "\n", " ", ""]
+)
+
+# splitting data from youtube video using text splitter and tokenizer
+texts = text_splitter.split_documents(data)
+
+#defining hugging face embedding variable
+hf_embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+#initializing vector database "Chroma db"
+vector_store = Chroma(
+    collection_name="data_collection",
+    embedding_function=hf_embeddings,
+)
+
+#storing data into documents variable to add to chroma db (vector database)
+
+documents= [
+    Document(
+        page_content=f"Source: {t.metadata['source']}, Title: {t.metadata['title']} \n\nContent: {t.page_content}",
+                   metadata=t.metadata
+                   )
+    for t in texts]
+
+#adding to database
+vectorstore_from_texts = vector_store.add_documents(documents=documents)
+
+# function to create vector embeddings of the user query 
+
+def get_embedding(text, model="sentence-transformers/all-MiniLM-L6-v2"):
+    response= HuggingFaceEmbeddings(model_name=model)
+    embedded_response = hf_embeddings.embed_query(text)
+    return embedded_response
+
+
+#making api
+# Define the QueryRequest model to accept a list of messages
+class Message(BaseModel):
+    role: str
+    content: str
+
+class QueryRequest(BaseModel):
+    messages: List[Message]
+
+
+
+@app.post('/api/ask/')
+
+
+#function for getting response from the llm
+async def get_response(request:QueryRequest):
+  print('api started running')
+  try:
+    all_messages= request.messages
+ #calling the function for user query vector embeddings
+    raw_query_embedding=get_embedding(all_messages[-1].content)
+    
+    #after converting query to vector, performing similarity search by vector 
+    results = vector_store.similarity_search_by_vector(
+        embedding=raw_query_embedding, k=1
+    )
+    
+    #providing the context to llm by performing similarity vector search
+    contexts = [doc.page_content for doc in results]
+    
+    #maintain user chat history
+    conversation_history= "\n\n".join(f"{msg.role}: {msg.content}" for msg in all_messages)
+    msg_string=json.dumps(conversation_history)
+    #combining context with user query to allow model to perform RAG
+    augmented_query = (
+            "<CONTEXT>\n" +
+            "\n\n-------\n\n".join(contexts) +
+            "\n-------\n</CONTEXT>\n\n\n\nMY QUESTION:\n" +
+            conversation_history
+        )
+    
+    # makng call to llm to get the answer
+    response = client.chat.completions.create(
+            model="llama3-8b-8192",
+            messages=[
+            {"role": "system", "content": primer},
+            {"role": "user", "content": augmented_query},
+            ],
+            max_tokens=1000,
+            temperature=1.2)
+    
+    #returning the response of llm
+    return {'assistantMessage':response.choices[0].message.content}
+  except Exception as e:
+      raise HTTPException(status_code=500,detail= str(e))
+
+
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,116 +1,164 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
 
-export default function Home() {
+import { Box, Button, Typography, Stack, TextField } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import SendIcon from '@mui/icons-material/Send';
+import ReactMarkdown from 'react-markdown'
+const Chatbot = () => {
+  // using useRef to scroll down automatically
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  // to store all the messages
+  const [allMessages, setAllMessages] = useState([
+    {
+      role: "assistant",
+      content: "Hi, I'm your culture and travel chat assistant. How can i help you today?",
+    },
+  ]);
+
+  // to take input from user and send it to backend and chaneg state
+  const [userMessage, setUserMessage] = useState("");
+
+  // function to call the api and get response + store it
+  const sendMessage = async () => {
+    setAllMessages((prevMessages) => [
+      ...prevMessages,
+      { role: "user", content: userMessage },
+      { role: "assistant", content: "" },
+    ]);
+
+
+
+  //   const response = await fetch("http://localhost:8000/test", {
+  //     method: "POST",
+  //     // headers: {
+  //     //     "Content-Type": "application/json",
+  //     // },
+  //     body: JSON.stringify({ test: "data" }),
+  // });
+  // const result = await response.json();
+  // console.log(result);
+
+
+    // sene message function uses the state which was there at the time of
+    // function invoking so emptying this state does not affect the api call 'userMessage'
+    //as a result, the previous state holding user message is included in api request and this line is
+    //used to clear the input placeolder
+    setUserMessage("");
+    try{
+      const getResponse = await fetch("http://localhost:8000/api/ask/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({messages:[
+          ...allMessages,
+          { role: "user", content: userMessage },
+        ]}),
+      });
+      const result = await getResponse.json();
+      if (result && result.assistantMessage) {
+        // Update the conversation with the assistant's response
+        setAllMessages((prevMessages) => [
+          ...prevMessages.slice(0, -1), // Remove the placeholder message
+          { role: "assistant", content: result.assistantMessage },
+        ]);
+      } else {
+        console.error("Invalid response format:", result);
+      }
+    }catch(error){
+      console.error("Error sending message:", error);
+    }
+  };
+
+
+  useEffect(() => {
+    if(scrollContainerRef.current){
+        scrollContainerRef.current.scrollTop=scrollContainerRef.current.scrollHeight;
+    }
+  }, [allMessages]);
+
+  const handleClick = () => {
+    sendMessage();
+  };
+
+  const handleChange = (e:any) => {
+    setUserMessage(e.target.value);
+  };
+
+  const handleKeyDown = (e:any) => {
+    if (e.key === "Enter") {
+      sendMessage();
+    }
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <Link href="/api/python">
-            <code className="font-mono font-bold">api/index.py</code>
-          </Link>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box display="flex" flexDirection='column'
+     alignItems="center" justifyContent="center" height='100vh'  >
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <Box width='80%' height='80vh' mt={7} 
+      display="flex" flexDirection='column'
+       justifyContent="center" alignItems="center" border="2px solid purple">
+        <Stack ref={scrollContainerRef} 
+         direction="column" height="80%"
+          sx={{overflowY: "auto", gap:1, m:1}}
+        >
+          {allMessages.map((msg, index) => (
+
+            <Box
+              key={index}
+              sx={{
+                display:"flex",
+                bgcolor: msg.role === "user" ? "#694F8E" : "#B692C2",
+                alignSelf: msg.role === "user" ? "flex-end" : "flex-start",
+                p:1,
+                borderRadius:8,
+                maxWidth:'80%',
+              }}
+            >
+              <Box color="white" 
+              textAlign= {msg.role ==="user"? "right":"left"} mx={2} my={1} >
+                <ReactMarkdown >{msg.content}</ReactMarkdown>
+                </Box>
+            </Box>
+          ))}
+        </Stack>
+        
+          <Stack px={2} width="100%" direction="row" gap={1}>
+
+          <TextField
+          type="text"
+          value={userMessage}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          name="user"
+          
+          sx={{
+            flex: 9, // Allows the TextField to take up available space
+            minWidth: 0, // Prevents the TextField from exceeding its container
+            bgcolor:'#EEEEEE' ,
+           ':focused': {
+            border: '2px solid green',
+            // Border color when focused
+          },
+          }}
+          
         />
-      </div>
+        <Button onClick={handleClick} variant="contained" sx={{
+            flex: 1, // Allows the TextField to take up available space
+            minWidth: 0, // Prevents the TextField from exceeding its container
+            bgcolor:"#2E236C",
+            textTransform:"capitalize",
+            borderRadius:2,
+            gap:1,
+            ':hover':{
+                bgcolor:'#17153B'
+            }
+          }}><Typography>Send</Typography><SendIcon/></Button>
+          </Stack>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </Box>
+    </Box>
   );
-}
+};
+
+export default Chatbot;
